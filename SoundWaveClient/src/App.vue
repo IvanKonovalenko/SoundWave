07d2d042929eea8c1c20ab470372f3d9184267e3
IvanKonovalenko <ivangame67@gmail.com>
2024-03-01 22:24:53 +0300

    Add first components

---
+++ SoundWaveClient/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <my-header></my-header>
+        <div>
+            <my-topcharts></my-topcharts>
+            <my-musicserchresult></my-musicserchresult>
+        </div>
+        <my-footer>
+        </my-footer>
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+
+    },
+    methods:{
+
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ SoundWaveClient/src/components/MyHeader.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <svg>
+
+        </svg>
+        <my-player></my-player>
+        <my-search></my-search>
+        <my-auth></my-auth>
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+
+    },
+    methods:{
+
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>
