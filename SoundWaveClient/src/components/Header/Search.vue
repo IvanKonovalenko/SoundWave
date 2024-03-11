f6424ca402f7b818f7b63dad8cd628b35193ecf0
Horgaring <dimarastvorsev@mail.ru>
2024-03-11 20:51:18 +0300

    AddSearchComponent

--- SoundWaveClient/src/components/Header/Search.vue
+++ SoundWaveClient/src/components/Header/Search.vue
@@ -1,14 +1,9 @@
 <template>
-    <div>
-        <nav class="my-serach navbar bg-body-tertiary">
-         <div class="container-fluid">
-         <a class="navbar-brand">&#128270</a>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-         </div>
-        </nav>
+    <div class="search-container">
+        <v-icon  name="fa-search"  scale="2" />
+            <input class="input"/>
+        
+        <button class="btn-Search" @click="add">Search</button>
     </div>
 </template>
 
@@ -26,11 +21,38 @@ export default{
 </script>
 
 <style>
-    .my-serach{
+    .search-container{
+
+        height: 100%;
+        width: 40%;
+        padding: 7px 10px;
+        background-color: #4d4c4c;
+        border-radius: 15px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap:  10px;
+    }
+    .input{
+        color: white;
+        font-size: 25px;
+        width: 90%;
+        background-color: #717070;
+        border: none;
+        border-bottom: 4px solid white;
         border-radius: 10px;
-        background-color: rgb(125, 116, 116);
+        outline: none;
     }
-    .btn:hover{
-        background-color: blueviolet;
+    .btn-Search{
+        color: white;
+        border-radius: 10px;
+        font-size: 25px;
+        border: none;
+        padding: 0px 20px;
+        background-color: blue;
     }
+    .btn-Search:hover{
+        background-color: rgb(40, 72, 231);
+    }
+    
 </style>
--- SoundWaveClient/src/main.js
+++ SoundWaveClient/src/main.js
@@ -4,7 +4,12 @@ import components from './components'
 
 
 const app = createApp(App);
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import { FaSearch, RiZhihuFill } from "oh-vue-icons/icons";
 
+addIcons(FaSearch, RiZhihuFill);
+
+app.component("v-icon", OhVueIcon);
 components.forEach(component =>{
     app.component(component.name, component)});
 app.mount('#app');
