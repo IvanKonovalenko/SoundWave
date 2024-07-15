1616a6e2ec9d22a64eb2d8eef05d08090c3008d9
IvanKonovalenko <ivangame67@gmail.com>
2024-07-15 21:47:07 +0300

    change np

--- src/Server/Api/Program.cs
+++ src/Server/Api/Program.cs
@@ -1,5 +1,7 @@
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>options.UseNpgsql()); 
 var app = builder.Build();
 
 app.MapControllers();
--- src/Server/DataLayer/AppDbContext.cs
+++ src/Server/DataLayer/AppDbContext.cs
@@ -1,6 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 
 public class AppDbContext:DbContext{
+    public AppDbContext(DbContextOptions<AppDbContext> options):base(options){
+        
+    }
     public DbSet<User> Users { get; set; }
     public DbSet<Audio> Audios { get; set; }
     public DbSet<Playlist> Playlists { get; set; }
